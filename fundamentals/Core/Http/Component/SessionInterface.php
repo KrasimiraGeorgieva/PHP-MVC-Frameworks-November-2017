233b41ebf7aa6f550955bc847d1644e125fb1886
Krasimira Georgieva <krassi77bg@gmail.com>
2017-11-25 22:14:13 +0200

    Added task solution.

---
+++ fundamentals/Core/Http/Component/SessionInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Core\Http\Component;
+
+
+interface SessionInterface
+{
+    public function getAttribute(string $key):string;
+
+    public function setAttribute(string $key, string $value);
+}
