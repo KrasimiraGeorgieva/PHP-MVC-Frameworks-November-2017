a6fc81f3f1df4dd0a6cc9ee6293a30bb4fdf8a1d
Krasimira Georgieva <krassi77bg@gmail.com>
2017-11-28 23:52:01 +0200

    Initial commit.

---
+++ blog/src/SoftUniBlogBundle/Controller/UsersController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace SoftUniBlogBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use SoftUniBlogBundle\Entity\User;
+use SoftUniBlogBundle\Form\UserType;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class UsersController extends Controller
+{
+    /**
+     * @Route("register", name="user_register")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function registerAction(Request $request)
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            $password = $this->get('security.password_encoder')->encodePassword($user, $user->getPassword());
+            $user->setPassword($password);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+            return $this->redirectToRoute("security_login");
+        }
+        return $this->render(
+            'default/register.html.twig',
+            array('form' => $form->createView()));
+        //return $this->render("default/register.html.twig");
+    }
+
+    /**
+     * @Security("is_granted('IS_AUTHENTICATED_FULLY')")
+     * @Route("/profile", name="user_profile")
+     */
+    public function profileAction()
+    {
+        $user = $this->getUser();
+        return $this->render("user/profile.html.twig", ['user'=>$user]);
+    }
+}
