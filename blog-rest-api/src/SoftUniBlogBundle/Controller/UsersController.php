a914126d25d3dc16c83635a82d705935d40a4292
Krasimira Georgieva <krassi77bg@gmail.com>
2017-12-02 23:34:38 +0200

    Initial commit.

---
+++ blog-rest-api/src/SoftUniBlogBundle/Controller/UsersController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace SoftUniBlogBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use SoftUniBlogBundle\Entity\User;
+use SoftUniBlogBundle\Form\UserType;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\FormError;
+use Symfony\Component\HttpFoundation\Request;
+use SoftUniBlogBundle\Entity\Role;
+
+class UsersController extends Controller
+{
+    /**
+     * @Route("register", name="user_register")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function registerAction(Request $request)
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+
+//        if ($user->getPassword() != $user->getConfirm()){
+//            $form->addError(new FormError("Password mismatch"));
+//            return $this->render('default/register.html.twig', ['form' => $form->createView()]);
+//        }
+        if ($form->isSubmitted()&& $form->isValid()) {
+            $password = $this->get('security.password_encoder')->encodePassword($user, $user->getPassword());
+            $user->setPassword($password);
+
+            $roleRepository = $this->getDoctrine()->getRepository(Role::class);
+            $userRole = $roleRepository->findOneBy(['name' => 'ROLE_USER']);
+
+            $user->addRole($userRole);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+            return $this->redirectToRoute("security_login");
+        }
+        return $this->render(
+            'default/register.html.twig',
+            array('form' => $form->createView())
+        );
+
+    }
+
+    /**
+     * @Security("is_granted('IS_AUTHENTICATED_FULLY')")
+     * @Route("/profile", name="user_profile")
+     */
+    public function profileAction()
+    {
+        $user = $this->getUser();
+        return $this->render("user/profile.html.twig", ['user'=>$user]);
+    }
+}
